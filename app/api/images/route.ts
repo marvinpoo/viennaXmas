e4528097d3480d1fe0be31e8dc128445d50fe703
Marvin Borisch <coppnic@e.email>
2024-11-21 02:09:40 +0100

    Update route.ts

--- app/api/images/route.ts
+++ app/api/images/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server'
 import fs from 'fs'
 import path from 'path'
 
+export const dynamic = "force-static";
+
 export async function GET() {
   const photosDir = path.join(process.cwd(), 'public', 'photos')
   const files = fs.readdirSync(photosDir)
