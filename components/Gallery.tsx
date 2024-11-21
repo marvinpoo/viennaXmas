ff63cc0e1da542a2870fedfccf0985ede79ee0c9
Marvin Borisch <coppnic@e.email>
2024-11-21 02:06:08 +0100

    Update Gallery.tsx

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -10,7 +10,7 @@ export default function Gallery() {
 
   useEffect(() => {
     const fetchImages = async () => {
-      const res = await fetch('/api/images')
+      const res = await fetch('/data/images.json')
       const data = await res.json()
       setImages(data)
     }
