45f00b4b4863fee8ce0e8807bfeae4fe3257ee90
Marvin Borisch <coppnic@e.email>
2024-11-21 02:53:31 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  assetPrefix: './', // Add this line
+  assetPrefix: './',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
   assetPrefix: process.env.NODE_ENV === 'production' ? '/viennaXmas' : '',
   basePath: process.env.NODE_ENV === 'production' ? '/viennaXmas' : '',
-  trailingSlash: true,
 }
 
 module.exports = nextConfig
